f145072efbedb41c3fe791b419799dae71c4e67b
Lawrence Eby and sam kirsch <sam@>
2017-01-24 09:29:41 -0800

    Add functionality for spec 4, which outputs different results based on which movie is added to cart

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,16 @@
-var Movie = function(movieName, newReleaseStatus, price, time) {
+var movieArray = [];
+
+var Movie = function(movieName, newReleaseStatus, time) {
   this.movieName = movieName;
   this.newReleaseStatus = newReleaseStatus;
   this.movieTimes = [];
-  this.price = price;
+  this.price = 7;
   this.selectTime = time;
 };
+
+var Ticket = function(quantity, time, movie) {
+
+}
 
 Movie.prototype.pricemodifier = function(time, quantity) {
   var price = 7;
@@ -27,13 +33,36 @@ Movie.prototype.pricemodifier = function(time, quantity) {
 $(function(){
   $("#movies").submit(function(event){
     event.preventDefault();
+
+      });
+// Movie 1
+  $("#movie1submit").click(function(event){
+    var name = "The Adventures of Epiherodus";
+    var newReleaseStatus = true;
     var movie1Time = parseFloat($("#movie1").val());
     var movie1Quantity = parseFloat($("#movie1sum").val());
-    var movie1 = new Movie("The Adventures of Epiherodus", true, 7, movie1Time);
 
+    var ticket1 = new Ticket(movie1Time, movie1Quantity, name);
+    var movie1 = new Movie(name, newReleaseStatus, movie1Time);
 
+    $(".output").append(movie1.movieName + " " + " at "+ movie1.selectTime + " for $" + movie1.pricemodifier(movie1Time, movie1Quantity) +"<br>");
 
-    $(".output").text(movie1.movieName + " " + " at "+ movie1.selectTime + " for $" + movie1.pricemodifier(movie1Time, movie1Quantity));
+    $("#movie1").val(0)
+    $("#movie1sum").val(0)
+  })
+// Movie 2
+  $("#movie2submit").click(function(event){
+    var name = "Bright Lights, Tech City";
+    var newReleaseStatus = false;
+    var movie2Time = parseFloat($("#movie2").val());
+    var movie2Quantity = parseFloat($("#movie2sum").val());
 
-  });
+    var ticket1 = new Ticket(movie2Time, movie2Quantity, name);
+    var movie2 = new Movie(name, newReleaseStatus, movie2Time);
+
+    $(".output").append(movie2.movieName + " " + " at "+ movie2.selectTime + " for $" + movie2.pricemodifier(movie2Time, movie2Quantity) +"<br>");
+
+    $("#movie2").val(0)
+    $("#movie2sum").val(0)
+  })
 });
